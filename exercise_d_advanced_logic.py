38097ed67e31e3d625cefadab5d24c0d0c96241d
Kat Zdzienkowska <katzdzienkowska@kats-mbp.home>
2022-05-17 23:03:04 +0100

    homework version3

--- exercise_d_advanced_logic.py
+++ exercise_d_advanced_logic.py
@@ -3,10 +3,18 @@
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # 1. Print out a list of the even integers:
-
+for num in numbers:
+  if num % 2 == 0:
+    print(num)
+#prints 6, 2, 2, 6
 
 # 2. Print the difference between the largest and smallest value:
+diff = 0
 
+for number in numbers:
+    numbers.sort()
+    diff = numbers[-1] - numbers[0]
+    print(diff)
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
 
